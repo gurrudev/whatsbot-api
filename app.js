d660a6ddc9d176d58fc762b76d097e1ff13d8a36
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-02-11 22:09:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,9 @@ app.post("/webhook", (req, res) => {
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
       let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
       
-      if (msg_body == "hi"){
+      let start = ["hi", "Hi", "hello", "Hello", "start", "Start"]
+      
+      if (msg_body == start){
         msg_body = "Hello, how may i help you..."
       }
       axios({
