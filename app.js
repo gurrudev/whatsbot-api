3d1e5261de716cbe3c46e3ac1460232042c3f96e
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-02-12 15:42:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ app.post("/webhook", (req, res) => {
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
       let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
       
-      let msg = "";
-      let type = "";
+      var msg = "";
+      var type = "";
       
       
       if (msg_body == "hi" || msg_body == "Hi" || msg_body == "hello" || msg_body == "Hello" || msg_body == "start" || msg_body == "Start"){
