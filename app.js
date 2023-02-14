08ec5d37f095e6cf1428f8e605ff07eb47506d2f
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-02-14 10:35:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,9 +41,11 @@ app.post("/webhook", (req, res) => {
       
   
       if (msg_body == "👋"|| msg_body == "✋"|| msg_body == "hi" || msg_body == "Hi" || msg_body == "hello" || msg_body == "Hello" || msg_body == "start" || msg_body == "Start"){
-        msg_body = "*Hi there! I am your virtual asistant from Gainn Fintach* \n\nI can do following please type correct keyword.\n\n*Balance* - To know your account balance\n*Portfolio* - To know your holdings\n*Orders* - To know about your orders\n*Contact* - Contact with our customer care\n*Help* - To get additional help"
+        msg_body = "*Hi there! I am your virtual asistant from Gainn Fintach* \n\nI can do following please type correct keyword.\n\n*Balance* - To know your account balance\n*Portfolio* - To know your holdings\n*Orders* - To know about your orders\n*Contact* - Contact with our customer care\n*Document* - To get your document\n*Help* - To get additional help"
       }else if (msg_body == "Balance" || msg_body == "balance"){
         msg_body = "Dear, {i} your account balance is {i}"
+      }else if (msg_body == "Document" || msg_body == "document"){
+        msg_body = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
       }else if (msg_body == "Portfolio" || msg_body == "portfolio" ){
         msg_body = "This is For portfolio\n\n{i}\n{i}\n{i}\n{i}"
       }else if (msg_body == "Orders" || msg_body == "orders"){
